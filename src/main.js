3242af3d4939bff4acdc7406db235ffdb0c6fd44
◖Luna <luna2020.app@gmail.com>
2019-11-28 13:01:06 +0200

    Initial

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Buefy from 'buefy'
 
-Vue.config.productionTip = false
+// i18n
+import { i18n } from '@/plugins/i18n'
+import { Trans } from '@/plugins/i18n/Translation'
 
+Vue.prototype.$i18nRoute = Trans.i18nRoute.bind(Trans)
+
+// Buefy
+//import 'buefy/dist/buefy.css'  <== Default Buefy style
+import '@/assets/style/index.scss'
+Vue.use(Buefy)
+
+// Icons
+import VueFeather from 'vue-feather'
+Vue.use(VueFeather)
+
+// Render
 new Vue({
+  router,
+  i18n,
   render: h => h(App),
 }).$mount('#app')
+
+Vue.config.productionTip = false
